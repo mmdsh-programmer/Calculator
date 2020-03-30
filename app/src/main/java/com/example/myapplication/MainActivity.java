64e01addfa5b0310c88c6b7f06f2997f5946f3fa
Mohammad Shakeri <programmer.mmdsh@gmail.com>
2020-03-31 00:56:01 +0430

    enable multi operation

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,9 +11,158 @@ import android.widget.TextView;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    String num,operator,history;
+    TextView show_result;
+    TextView show_main_result;
+    double first,second;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        show_result = findViewById(R.id.result);
+        show_main_result = findViewById(R.id.mainResult);
+        num="";
+        history="";
+        operator = "";
+        second = 0;
+    }
+
+    public void onClick(View view){
+        Button button = (Button) view;
+        num+= button.getText().toString();
+        show_main_result.setText(num);
+        first = Double.parseDouble(num);
+    }
+
+    public void onSum(View view){
+        if (operator == ""){
+            second = first;
+        }
+        else{
+            if (operator == "+"){
+                second += first;
+            }
+            else if (operator == "-")
+            {
+                second-= first;
+            }
+            else if(operator == "*")
+            {
+                second *= first;
+            }
+            else if (operator == "/")
+            {
+                second /= first;
+            }
+        }
+        operator="+";
+        history += num + "+";
+        show_result.setText(history);
+        num="";
+        show_main_result.setText("");
+    }
+
+    public void onMinus(View view){
+        if (operator == ""){
+            second = first;
+        }
+        else{
+            if (operator == "+"){
+                second += first;
+            }
+            else if (operator == "-")
+            {
+                second-= first;
+            }
+            else if(operator == "*")
+            {
+                second *= first;
+            }
+            else if (operator == "/")
+            {
+                second /= first;
+            }
+        }
+        operator="-";
+        history += num + "-";
+        show_result.setText(history);
+        num="";
+        show_main_result.setText("");
+    }
+
+    public void onMultiply(View view){
+        if (operator == ""){
+            second = first;
+        }
+        else{
+            if (operator == "+"){
+                second += first;
+            }
+            else if (operator == "-")
+            {
+                second-= first;
+            }
+            else if(operator == "*")
+            {
+                second *= first;
+            }
+            else if (operator == "/")
+            {
+                second /= first;
+            }
+        }
+        operator="*";
+        history += num + "*";
+        show_result.setText(history);
+        num="";
+        show_main_result.setText("");
+    }
+
+    public void onDivide(View view){
+        if (operator == ""){
+            second = first;
+        }
+        else{
+            if (operator == "+"){
+                second += first;
+            }
+            else if (operator == "-")
+            {
+                second-= first;
+            }
+            else if(operator == "*")
+            {
+                second *= first;
+            }
+            else if (operator == "/")
+            {
+                second /= first;
+            }
+        }
+        operator="/";
+        history += num + "/";
+        show_result.setText(history);
+        num="";
+        show_main_result.setText("");
+    }
+
+    public void onEqual(View view){
+        if (operator == "+"){
+            second += first;
+        }
+        else if (operator == "-")
+        {
+            second-= first;
+        }
+        else if(operator == "*")
+        {
+            second *= first;
+        }
+        else if (operator == "/")
+        {
+            second /= first;
+        }
+        show_result.setText(history + first + "=");
+        show_main_result.setText(Double.toString(second));
     }
 }
